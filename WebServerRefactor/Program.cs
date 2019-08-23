eb30502d4d7a26a01d15bd399d470c6202d0b156
tavisca-akadam <ankadam@tavisca.com>
2019-08-23 22:51:40 +0530

    Getting User request done

---
+++ WebServerRefactor/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace WebServerRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WebServer webServer = new WebServer();
+            Console.ReadKey();
+        }
+    }
+    public class WebServer
+    {
+        private TcpListener _listener;
+        private int port = 5555;
+
+        public WebServer()
+        {
+            try
+            {
+                _listener = new TcpListener(port);
+                _listener.Start();
+                Console.WriteLine("Web Server Running Press CTRL + C to quit.....");
+
+                Thread deamon = new Thread(new ThreadStart(StartListen));
+                deamon.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An Exception Occurred while Listening :" + e.ToString());
+            }
+        }
+        public void StartListen()
+        {
+            while(true)
+            {
+                Socket socket = _listener.AcceptSocket();
+                Console.WriteLine("Socket Type :" + socket.SocketType);
+                if(socket.Connected)
+                {
+                    Console.WriteLine("Client Connected \n Client IP " + socket.RemoteEndPoint);
+                    Parser.TryParse(socket);
+                }
+            }
+        }
+    }
+    public class Parser
+    {
+
+        public static void TryParse(Socket conn)
+        {
+            byte[] receivedBuffer = new byte[1024];
+            int messageLength = conn.Receive(receivedBuffer, receivedBuffer.Length, 0);
+            string messageBuffer = Encoding.ASCII.GetString(receivedBuffer);
+
+            Console.WriteLine($"messageLength : {messageLength}, messageBuffer : {messageBuffer}");
+
+        }
+    }
+}
---
+++ WebServerTest/ConnectionHandlerFixture.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+using WebServerRefactor;
+using System.Net.Sockets;
+
+namespace WebServerTest
+{
+    public class ConnectionHandlerFixture
+    {
+        
+    }
+}
